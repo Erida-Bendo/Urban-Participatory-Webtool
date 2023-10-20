cc22264200f7a3188ff6c2ededf1a1176d37e72d
Erida Bendo <erida.bendo@burohappold.com>
2023-10-20 15:28:57 +0200

    final frontend

--- bundle.js
+++ bundle.js
@@ -29,6 +29,7 @@ function removeMarkers(){
     for (var i = markers.length - 1; i >= 0; i--) {
         markers[i].remove();
       }
+    markers=[];
 }
 
 function getMarkerCoordinates(type){
@@ -43,10 +44,12 @@ function addLogo(string){
     logo.src=string;
 }
 
+var extraInput;
 
 function func1(){
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    info.style.height="20%";
+    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo gibt es, Ihrer Meinung nach, einen Bedarf an Trinkplätzen in Burscheid?<br><br><b>Verschieben Sie die 5 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/drinking.png");
     createMarkers(5, "#AED6F1");
@@ -55,16 +58,16 @@ function func2(){
     getMarkerCoordinates("drinking spot");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Fontänen können den Komfort im Freien durch Verdunstungskühlung verbessern.</b><br> Wo gibt es, Ihrer Meinung nach, einen Bedarf an Fontänen in Burscheid?<br><br><b>Verschieben Sie die 4 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/fountain.png");
-    createMarkers(3,"#17A589");
+    createMarkers(4,"#17A589");
 }
 function func3(){
     getMarkerCoordinates("fountain");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Vernebelungsanlagen sind eine Strategie zur Bekämpfung von Hitzestress, insbesondere dort, wo sich viele Menschen zusammenkommen oder vorbeigehen.</b><br> Wo gibt es, Ihrer Meinung nach, einen Bedarf an Vernebelungsanlagen in Burscheid ?<br><b>Verschieben Sie die 4 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/misting.png");
     createMarkers(4, "#1A5276");
@@ -74,7 +77,7 @@ function func4(){
     getMarkerCoordinates("misting");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Öffentliche Toiletten ermöglichen freie Bewegung in öffentlichen Räumen.</b><br>Wo gibt es, Ihrer Meinung nach, einen Bedarf an öffentlichen Toiletten in Burscheid?<br><br><b>Verschieben Sie die 4 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/restroom.png");
     createMarkers(4, "#212F3D");
@@ -84,7 +87,7 @@ function func5(){
     getMarkerCoordinates("restroom");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Sitzbänke unterstützten gesunde Alltagsroutinen.</b><br> Wo werden Ihrer Meinung nach Sitzbänke am meisten gebraucht?<br><br><b>Verschieben Sie die 10 Markierungen in der Karte an die gewünschten Stellen.. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/benches.png");
     createMarkers(10, "#F5CBA7");
@@ -94,16 +97,16 @@ function func6(){
     getMarkerCoordinates("benches");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Das Straßennetz und die hügelige Landschaft erschweren das Gehen zu Fuß. Bei Hitzewellen erhöht sich dadurch das Auftreten von Hitzeanfällen. </b><br> Wo gibt es, Ihrer Meinung nach, einen Bedarf an Rampen in Burscheid?<br><br><b>Verschieben Sie die 5 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/accessibility.png");
-    createMarkers(10, "#EC7063");
+    createMarkers(5, "#EC7063");
 }
 
 function func7(){
     getMarkerCoordinates("accesibility spot");
     removeMarkers();
-    intro.innerHTML = "<b>Bäume sind die beste Beschattungsstrategie, die auch Verdunstungskälte bietet.</b><br> Wo werden Ihrer Meinung nach Bäume in der Stadt am meisten gebraucht? <br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
+    intro.innerHTML = "<b>Die Anpflanzung von Bäumen sind die beste Beschattungsstrategie, die auch Verdunstungskälte bietet.</b><br>Wo werden Ihrer Meinung nach Bäume in der Stadt am meisten gebraucht? <br><br><b>Verschieben Sie die 15 Markierungen in der Karte an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
     addLogo("/icons/Trees.png");
     createMarkers(15, "#229954");
 }
@@ -111,38 +114,94 @@ function func7(){
 function func8(){
     getMarkerCoordinates("tree");
     removeMarkers();
-    intro.innerHTML = "<b>Bäume sind die beste Beschattungsstrategie, die auch Verdunstungskälte bietet.</b><br> Wo werden Ihrer Meinung nach Bäume in der Stadt am meisten gebraucht? <br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
+    intro.innerHTML = "<b>Spielplätze sind ein wichtiges Umfeld für Kinder und junge Familien.</b><br> Wo werden Ihrer Meinung nach Spielplätze am meisten gebraucht? <br><br><b>Verschieben Sie die 5 Markierungen in der Karte an die gewünschten Stellen.. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
     addLogo("/icons/playground.png");
-    createMarkers(7, "#F4D03F");
+    createMarkers(5, "#F4D03F");
 }
 
 function func9(){
     getMarkerCoordinates("playground");
     removeMarkers();
-    intro.innerHTML = "Danke für Ihre Teilnahme. Falls Sie wünschen können sie auch einen Kommentar einreichen.";
+    intro.innerHTML = "Gibt es noch etwas, das Ihrer Meinung nach in Burscheid fehlt?<br><br><b>Schreiben Sie den Namen unten und dann verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
+    
     logo.src="";
+
+    extraInput = document.createElement("input");
+    extraInput.type = "text"; 
+    extraInput.id="extra";
+
+    info.appendChild(extraInput);
+    createMarkers(5, "#808B96");
+}
+
+function func10(){
+    getMarkerCoordinates(extraInput.value);
+    removeMarkers();
+    intro.innerHTML = "Danke für Ihre Teilnahme. Falls Sie wünschen können sie auch einen Kommentar einreichen.";
+    extraInput.remove();
     button.remove();
 
-    var newInput = document.createElement("input");
-    newInput.type = "text"; 
-    newInput.id="input";
+    input1=document.createElement("div");
+    input1.id="inputs";
+    info.appendChild(input1);
 
+    var age = document.createElement("h");
+    age.textContent="Alter";
+    var ageInput= document.createElement("input");
+    ageInput.type="text";
+    ageInput.id="input";
+
+    input2=document.createElement("div");
+    input2.id="inputs";
+    info.appendChild(input2);
+
+
+    input3=document.createElement("div");
+    input3.id="inputs";
+    info.appendChild(input3);
+
+    var comment = document.createElement("h");
+    comment.textContent="Kommentar";
+    var commentInput = document.createElement("input");
+    commentInput.type = "text"; 
+    commentInput.id="input";
+
+
+    input1.appendChild(age);
+    input1.appendChild(ageInput);
+    input3.appendChild(comment);
+    input3.appendChild(commentInput);
+
+    
     var endButton = document.createElement("button");
     endButton.textContent = "Einreichen";
     endButton.id= "button";
     endButton.onclick = () =>{
-        console.log(newInput.value);
-        intro.innerHTML = "Danke für Ihre Teilnahme.";
+        intro.innerHTML = "Danke für Ihre Teilnahme. <br> <br><b>Ihre Daten wurden gesammelt.<b/> <br><br><br><br><i>Unter den folgenden Link können Sie mehr über Hitzeschütz und Hitzevorsorge in Deutschland lesen<i/><br/>";
+        
+        var link = document.createElement("a");
+
+        // Set the href attribute to specify the URL
+        link.href = "https://www.staedtetag.de/themen/klimaschutz-und-energie/hitzeschutz-hitzevorsorge-staedte";
+
+        // Set the text content for the link
+        link.textContent = "Hitzeschutz und Hitzevorsorge in den Städten";
+        intro.appendChild(link);
+
+
         endButton.remove();
-        newInput.remove();
+        input1.remove();
+        input2.remove();
+        input3.remove();
+        console.log(ageInput.value);
+        console.log(genderInput.value);
+        console.log(commentInput.value);
     };
 
-    info.appendChild(newInput);
     info.appendChild(endButton);
-
 }
 
-const funcs = [func1, func2, func3, func4, func5, func6, func7, func8, func9];
+const funcs = [func1, func2, func3, func4, func5, func6, func7, func8, func9, func10];
 let i = 0;
 button.addEventListener("click", e => {
     funcs[i]();
--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function removeMarkers(){
     for (var i = markers.length - 1; i >= 0; i--) {
         markers[i].remove();
       }
+    markers=[]
 }
 
 function getMarkerCoordinates(type){
@@ -43,6 +44,7 @@ function addLogo(string){
     logo.src=string
 }
 
+var extraInput;
 
 function func1(){
     intro.innerHTML = intro.textContent.trim();
@@ -55,16 +57,16 @@ function func2(){
     getMarkerCoordinates("drinking spot");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Fontänen können den Komfort im Freien durch Verdunstungskühlung verbessern.</b><br> Wo würden Sie Fontänen in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/fountain.png");
-    createMarkers(3,"#17A589");
+    createMarkers(4,"#17A589");
 }
 function func3(){
     getMarkerCoordinates("fountain");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Vernebelungsanlagen sind eine Strategie zur Bekämpfung von Hitzestress, insbesondere dort, wo sich viele Menschen zusammenkommen oder vorbeigehen.</b><br> Wo würden Sie Vernebelungsanlagen in Burscheid platzieren?<br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/misting.png")
     createMarkers(4, "#1A5276");
@@ -74,7 +76,7 @@ function func4(){
     getMarkerCoordinates("misting");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Öffentliche Toiletten ermöglichen freie Bewegung in öffentlichen Räumen.</b><br> Wo würden Sie öffentliche Toiletten in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/restroom.png")
     createMarkers(4, "#212F3D");
@@ -84,7 +86,7 @@ function func5(){
     getMarkerCoordinates("restroom");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Sitzbänke unterstützten gesunde Alltagsroutinen.</b><br> Wo werden Ihrer Meinung nach Sitzbänke am meisten gebraucht??<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/benches.png")
     createMarkers(10, "#F5CBA7");
@@ -94,10 +96,10 @@ function func6(){
     getMarkerCoordinates("benches");
     removeMarkers();
     intro.innerHTML = intro.textContent.trim();
-    intro.innerHTML = "<b>Bei Hitzewellen ist es wichtig, Trinkplätze anzubieten.</b><br> Wo würden Sie die Trinkplätze in Burscheid platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
+    intro.innerHTML = "<b>Das Straßennetz und die hügelige Landschaft erschweren das Gehen zu Fuß.</b><br> Wo würden Sie Rampen platzieren?<br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen ";
     button.innerHTML="Zum nächsten Schritt";
     addLogo("/icons/accessibility.png")
-    createMarkers(10, "#EC7063");
+    createMarkers(5, "#EC7063");
 }
 
 function func7(){
@@ -111,38 +113,90 @@ function func7(){
 function func8(){
     getMarkerCoordinates("tree");
     removeMarkers();
-    intro.innerHTML = "<b>Bäume sind die beste Beschattungsstrategie, die auch Verdunstungskälte bietet.</b><br> Wo werden Ihrer Meinung nach Bäume in der Stadt am meisten gebraucht? <br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
-    addLogo("/icons/playground.png")
-    createMarkers(7, "#F4D03F");
+    intro.innerHTML = "<b>Spielplätze sind ein wichtiges Umfeld für Kinder und junge Familien.</b><br> Wo werden Ihrer Meinung nach Spielplätze am meisten gebraucht? <br><br><b>Verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
+    addLogo("/icons/playground.png");
+    createMarkers(5, "#F4D03F");
 }
 
 function func9(){
     getMarkerCoordinates("playground");
     removeMarkers();
-    intro.innerHTML = "Danke für Ihre Teilnahme. Falls Sie wünschen können sie auch einen Kommentar einreichen.";
+    intro.innerHTML = "Gibt es noch etwas, das Ihrer Meinung nach in Burscheid fehlt?<br><br><b>Schreiben Sie den Namen unten und dann verschieben Sie die Markierungen an die gewünschten Stellen. Danach klicken Sie auf die Schaltfläche unten, um zum nächsten Schritt zu gelangen";
+    
     logo.src="";
+
+    extraInput = document.createElement("input");
+    extraInput.type = "text"; 
+    extraInput.id="extra";
+
+    info.appendChild(extraInput);
+    createMarkers(5, "#808B96");
+}
+
+function func10(){
+    getMarkerCoordinates(extraInput.value);
+    removeMarkers();
+    intro.innerHTML = "Danke für Ihre Teilnahme. Falls Sie wünschen können sie auch einen Kommentar einreichen.";
+    extraInput.remove();
     button.remove();
 
-    var newInput = document.createElement("input");
-    newInput.type = "text"; 
-    newInput.id="input";
+    input1=document.createElement("div");
+    input1.id="inputs";
+    info.appendChild(input1);
 
+    var age = document.createElement("h");
+    age.textContent="Alter"
+    var ageInput= document.createElement("input");
+    ageInput.type="text";
+    ageInput.id="input";
+
+    input2=document.createElement("div");
+    input2.id="inputs";
+    info.appendChild(input2);
+
+    var gender = document.createElement("h");
+    gender.textContent="Geschlecht"
+    var genderInput= document.createElement("input");
+    genderInput.type="text";
+    genderInput.id="input";
+
+    input3=document.createElement("div");
+    input3.id="inputs";
+    info.appendChild(input3);
+
+    var comment = document.createElement("h");
+    comment.textContent="Kommentar"
+    var commentInput = document.createElement("input");
+    commentInput.type = "text"; 
+    commentInput.id="input";
+
+
+    input1.appendChild(age);
+    input1.appendChild(ageInput);
+    input2.appendChild(gender);
+    input2.appendChild(genderInput);
+    input3.appendChild(comment);
+    input3.appendChild(commentInput);
+
+    
     var endButton = document.createElement("button");
     endButton.textContent = "Einreichen";
     endButton.id= "button";
     endButton.onclick = () =>{
-        console.log(newInput.value)
-        intro.innerHTML = "Danke für Ihre Teilnahme.";
+        intro.innerHTML = "Danke für Ihre Teilnahme. <br> <b>Ihre Daten wurden gesammelt.<b/>";
         endButton.remove();
-        newInput.remove();
+        input1.remove();
+        input2.remove();
+        input3.remove();
+        console.log(ageInput.value)
+        console.log(genderInput.value)
+        console.log(commentInput.value)
     }
 
-    info.appendChild(newInput);
     info.appendChild(endButton);
-
 }
 
-const funcs = [func1, func2, func3, func4, func5, func6, func7, func8, func9];
+const funcs = [func1, func2, func3, func4, func5, func6, func7, func8, func9, func10];
 let i = 0;
 button.addEventListener("click", e => {
     funcs[i]();
